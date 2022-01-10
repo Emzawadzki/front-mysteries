3b41dc9d8b7b9dd29b0eb36cc1a5d3dc485165db
Emzawadzki <emzawadzki@gmail.com>
2022-01-10 21:14:31 +0100

    feat: add server caching

--- caching/client/app.js
+++ caching/client/app.js
@@ -58,5 +58,43 @@ const attachClientCacheListeners = () => {
   })
 }
 
+const attachServerCacheListeners = () => {
+  const sendBtn = document.getElementById("js-server-cache-send");
+  const firstReqDd = document.getElementById("js-first-server-cache-dd");
+  const followingReqDd = document.getElementById("js-folowing-server-cache-dd");
+  const timer = document.getElementById("js-server-cache-timer");
+
+  let timerInterval;
+  let timeCounter;
+  let requestsSent = 0;
+
+  const resetTimer = () => {
+    if (timerInterval) clearInterval(timerInterval);
+    timeCounter = 30;
+    timer.innerText = `${timeCounter}s`;
+    timerInterval = setInterval(updateTimer, 1_000);
+  }
+
+  const updateTimer = () => {
+    if (timeCounter > 0) {
+      timeCounter--;
+      timer.innerText = `${timeCounter}s`;
+    }
+  }
+
+  sendBtn.addEventListener('click', async () => {
+    requestsSent++;
+    const res = await fetch("/server-cache");
+    const requestCount = (await res.json()).count;
+    if (requestsSent === 1) {
+      resetTimer();
+      firstReqDd.innerText = requestCount;
+      return;
+    }
+    followingReqDd.innerText = requestCount;
+  })
+}
+
 attachNoCacheListeners();
-attachClientCacheListeners()
+attachClientCacheListeners();
+attachServerCacheListeners();
