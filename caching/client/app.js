106bc966162eecb97aba870d0e5887a6908380e4
Emzawadzki <emzawadzki@gmail.com>
2022-01-10 20:33:58 +0100

    feat: add no-cache requests

--- caching/client/app.js
+++ caching/client/app.js
@@ -1,1 +1,20 @@
-console.log("I'm in")
+const attachNoCacheListeners = () => {
+  const sendBtn = document.getElementById("js-no-cache-send");
+  const firstReqDd = document.getElementById("js-first-no-cache-dd");
+  const followingReqDd = document.getElementById("js-folowing-no-cache-dd");
+
+  let requestSent = 0;
+
+  sendBtn.addEventListener('click', async () => {
+    requestSent++;
+    const res = await fetch("/no-cache");
+    const requestCount = (await res.json()).count;
+    if (requestSent === 1) {
+      firstReqDd.innerText = requestCount;
+      return;
+    }
+    followingReqDd.innerText = requestCount;
+  })
+}
+
+attachNoCacheListeners();
