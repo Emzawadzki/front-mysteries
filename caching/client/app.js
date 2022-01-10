4957677063e78f38dd3389170e38df8050ff4660
Emzawadzki <emzawadzki@gmail.com>
2022-01-10 21:06:58 +0100

    feat: add client cache

--- caching/client/app.js
+++ caching/client/app.js
@@ -3,13 +3,54 @@ const attachNoCacheListeners = () => {
   const firstReqDd = document.getElementById("js-first-no-cache-dd");
   const followingReqDd = document.getElementById("js-folowing-no-cache-dd");
 
-  let requestSent = 0;
+  let requestsSent = 0;
 
   sendBtn.addEventListener('click', async () => {
-    requestSent++;
+    requestsSent++;
     const res = await fetch("/no-cache");
     const requestCount = (await res.json()).count;
-    if (requestSent === 1) {
+    if (requestsSent === 1) {
+      firstReqDd.innerText = requestCount;
+      return;
+    }
+    followingReqDd.innerText = requestCount;
+  })
+}
+
+const attachClientCacheListeners = () => {
+  const sendBtn = document.getElementById("js-client-cache-send");
+  const firstReqDd = document.getElementById("js-first-client-cache-dd");
+  const followingReqDd = document.getElementById("js-folowing-client-cache-dd");
+  const timer = document.getElementById("js-client-cache-timer");
+
+  let timerInterval;
+  let timeCounter;
+  let requestsSent = 0;
+
+  const resetTimer = () => {
+    if (timerInterval) clearInterval(timerInterval);
+    timeCounter = 30;
+    timer.innerText = `${timeCounter}s`;
+    timerInterval = setInterval(updateTimer, 1_000);
+  }
+
+  const updateTimer = () => {
+    if (timeCounter > 0) {
+      timeCounter--;
+      timer.innerText = `${timeCounter}s`;
+    }
+  }
+
+  sendBtn.addEventListener('click', async () => {
+    requestsSent++;
+    const res = await fetch("/client-cache", {
+      headers: {
+        "Cache-Control": "max-age=30"
+      }
+    });
+    const requestCount = (await res.json()).count;
+    if (requestsSent === 1) {
+      resetTimer();
       firstReqDd.innerText = requestCount;
       return;
     }
@@ -18,3 +59,4 @@ const attachNoCacheListeners = () => {
 }
 
 attachNoCacheListeners();
+attachClientCacheListeners()
